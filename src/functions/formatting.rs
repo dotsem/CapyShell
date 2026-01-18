c76ab3dcdfb04537206380618cd556c1c9dc5583
Sem Van Broekhoven <144097969+dotsem@users.noreply.github.com>
2026-01-18 14:45:08 +0100

    update formatting function to use clear syntax

--- src/functions/formatting.rs
+++ src/functions/formatting.rs
@@ -1,11 +1,18 @@
+#[inline]
 pub fn int_to_readable_byte_string(bytes: u64) -> String {
-    if bytes < 1024 {
-        format!("{} B", bytes)
+    let (value, unit) = if bytes < 1024 {
+        (bytes as f32, "B")
     } else if bytes < 1024 * 1024 {
-        format!("{:.1} KB", bytes as f32 / 1024.0)
+        (bytes as f32 / 1024.0, "KB")
     } else if bytes < 1024 * 1024 * 1024 {
-        format!("{:.1} MB", bytes as f32 / (1024.0 * 1024.0))
+        (bytes as f32 / (1024.0 * 1024.0), "MB")
     } else {
-        format!("{:.1} GB", bytes as f32 / (1024.0 * 1024.0 * 1024.0))
+        (bytes as f32 / (1024.0 * 1024.0 * 1024.0), "GB")
+    };
+
+    if unit == "B" {
+        format!("{value} {unit}")
+    } else {
+        format!("{value:.1} {unit}")
     }
 }
